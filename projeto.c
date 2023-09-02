11d7c1ce67126daaf49ba105d3fee410f26bb846
AsuraTheArtistz <122319732+AsuraTheArtistz@users.noreply.github.com>
2023-09-02 19:41:47 -0300

    Calculator.c

--- projeto.c
+++ projeto.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
- int main()
- {
-   int numero;
-   int numero2;
-   char escolha;
+int main()
+{
+    int number; // Declare an integer variable for the first number.
+    int number2; // Declare an integer variable for the second number.
+    char choice; // Declare a character variable for the operation choice.
 
-   printf("Digite um numero: \n");
-   scanf("%d", &numero);
+    printf("Enter a number: \n"); // Prompt the user to enter a number.
+    scanf("%d", &number); // Read the first number entered by the user.
 
-   printf("Digite um segundo numero: \n");
-   scanf("%d", &numero2);
+    printf("Enter a second number: \n"); // Prompt the user to enter a second number.
+    scanf("%d", &number2); // Read the second number entered by the user.
 
-   printf("Digite a operacao que voce deseja (+, -, *, /): \n"); 
-   scanf(" %c", &escolha);
+    printf("Enter the operation you want (+, -, *, /): \n"); // Prompt the user to enter an arithmetic operation.
+    scanf(" %c", &choice); // Read the operation choice entered by the user.
 
-   
-   if (escolha == '+') {
-      printf("O resultado e: %d\n", numero + numero2);
-   } else if (escolha == '-') {
-      printf("O resultado e: %d\n", numero - numero2);
-   } else if (escolha == '*') {
-      printf("O resultado e: %d\n", numero * numero2);
-   } else if (escolha == '/') {
-      printf("O resultado e: %d\n", numero / numero2);
-   }
-   
-   return 0;
+    if (choice == '+') {
+        printf("The result is: %d\n", number + number2); // Add the two numbers and print the result.
+    } else if (choice == '-') {
+        printf("The result is: %d\n", number - number2); // Subtract the second number from the first and print the result.
+    } else if (choice == '*') {
+        printf("The result is: %d\n", number * number2); // Multiply the two numbers and print the result.
+    } else if (choice == '/') {
+        printf("The result is: %d\n", number / number2); // Divide the first number by the second and print the result.
+    }
 
-
- }
+    return 0;
+}
